72ef87c0a6312c98606c48bdede340b5711c1330
bahtiyar212 <bahtiyardundar03@gmail.com>
2022-09-17 23:21:20 +0300

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import React from "react";
+import Header from "./compenents/header";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Header />
     </div>
   );
 }
