232226e4eda3c4813dff58cdf7554de6515f645d
bahtiyar212 <bahtiyardundar03@gmail.com>
2022-09-25 10:14:32 +0300

    new

--- src/compenents/header.js
+++ src/compenents/header.js
@@ -4,17 +4,17 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
-const Header=(props)=>{
+const Header=(props)=> {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            HOSPITAL
+            HOSPİTAL
           </Typography>
         </Toolbar>
       </AppBar>
     </Box>
   );
 }
-export default Header;
+export default Header
